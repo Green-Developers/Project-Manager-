afe53099dba6590c338a9040acb03871cb5dcafb
arshiammz <mohammadzade.arshia84@gmail.com>
2024-12-26 00:31:34 +0330

    CircularQueue Data Structure created.

--- Backend/Data Structures.py
+++ Backend/Data Structures.py
@@ -196,3 +196,59 @@ class Stack:
             prev = current
             current = next_node
         self.top = prev
+# --------------------------------------------------------------------------------------
+class CircularQueue:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.queue = [None] * capacity
+        self.front = -1
+        self.rear = -1
+
+    def is_empty(self):
+        return self.front == -1
+
+    def is_full(self):
+        return (self.rear + 1) % self.capacity == self.front
+
+    def enqueue(self, item):
+        if self.is_full():
+            raise OverflowError("Queue is full")
+
+        if self.is_empty():
+            self.front = 0
+        self.rear = (self.rear + 1) % self.capacity
+        self.queue[self.rear] = item
+
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+
+        item = self.queue[self.front]
+        self.queue[self.front] = None
+
+        if self.front == self.rear:
+            self.front = -1
+            self.rear = -1
+        else:
+            self.front = (self.front + 1) % self.capacity
+
+        return item
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        return self.queue[self.front]
+
+    def size(self):
+        if self.is_empty():
+            return 0
+        if self.rear >= self.front:
+            return self.rear - self.front + 1
+        return self.capacity - self.front + self.rear + 1
+
+    def __str__(self):
+        if self.is_empty():
+            return "Queue is empty"
+        if self.rear >= self.front:
+            return " -> ".join(map(str, self.queue[self.front: self.rear + 1]))
+        return " -> ".join(map(str, self.queue[self.front:] + self.queue[: self.rear + 1]))
