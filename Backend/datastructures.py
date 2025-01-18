8caca67d47921281ebec1355dd1aba85ce618c62
Zeinabc7 <darya.abbasi1383@gmail.com>
2025-01-18 21:45:09 +0330

    ✅

---
+++ Backend/datastructures.py
@@ -0,0 +1,340 @@
+class Node:
+    def __init__(self, value, priority=0):
+        self.value = value
+        self.priority = priority
+        self.next = None
+
+# -------------------------------------------------------------------------------------
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def insert(self, index, value):
+        new_node = Node(value)
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            prev = None
+            i = 0
+            while current and i < index:
+                prev = current
+                current = current.next
+                i += 1
+            if i == index:
+                prev.next = new_node
+                new_node.next = current
+            else:
+                raise IndexError("Index out of range")
+
+    def remove(self, value):
+        current = self.head
+        prev = None
+        while current:
+            if current.value == value:
+                if prev:
+                    prev.next = current.next
+                else:
+                    self.head = current.next
+                return
+            prev = current
+            current = current.next
+        raise ValueError(f"{value} not found in the list")
+
+    def pop(self, index=0):
+        if not self.head:
+            raise IndexError("Pop from empty list")
+        current = self.head
+        prev = None
+        i = 0
+        while current and i < index:
+            prev = current
+            current = current.next
+            i += 1
+        if current:
+            if prev:
+                prev.next = current.next
+            else:
+                self.head = current.next
+            return current.value
+        else:
+            raise IndexError("Index out of range")
+
+    def __len__(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    def __getitem__(self, index):
+        current = self.head
+        i = 0
+        while current:
+            if i == index:
+                return current.value
+            current = current.next
+            i += 1
+        raise IndexError("Index out of range")
+
+    def __repr__(self):
+        values = []
+        current = self.head
+        while current:
+            values.append(current.value)
+            current = current.next
+        return " -> ".join(map(str, values))
+
+# -------------------------------------------------------------------------------------
+class QueueNode:
+    def __init__(self, value, priority):
+        self.value = value  # مقدار (مثلاً یک شیء تسک)
+        self.priority = priority  # اولویت (مثلاً end_date)
+        self.next = None  # اشاره‌گر به گره بعدی
+
+class PriorityQueue:
+    def __init__(self):
+        self.front = None  # گره اول صف
+
+    def enqueue(self, task):
+        """
+        اضافه کردن تسک به صف بر اساس اولویت (end_date نزدیک‌تر بالاترین اولویت را دارد)
+        """
+        priority = task.end_date
+        new_node = QueueNode(task, priority)
+
+        if not self.front or self.front.priority > priority:
+            new_node.next = self.front
+            self.front = new_node
+        else:
+            current = self.front
+            while current.next and current.next.priority <= priority:
+                current = current.next
+            new_node.next = current.next
+            current.next = new_node
+
+    def dequeue(self):
+        """
+        حذف و بازگشت به بالاترین اولویت
+        """
+        if not self.front:
+            raise IndexError("Dequeue from empty priority queue")
+        task = self.front.value
+        self.front = self.front.next
+        return task
+
+    def peek(self):
+        """
+        مشاهده بالاترین اولویت بدون حذف
+        """
+        if not self.front:
+            raise IndexError("Peek from empty priority queue")
+        return self.front.value
+
+    def is_empty(self):
+        """
+        بررسی خالی بودن صف
+        """
+        return self.front is None
+
+    def __len__(self):
+        """
+        شمارش تعداد تسک‌ها در صف
+        """
+        count = 0
+        current = self.front
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    def __repr__(self):
+        """
+        نمایش صف به صورت رشته برای بررسی
+        """
+        values = []
+        current = self.front
+        while current:
+            task = current.value
+            values.append(f"{task.name}(end: {task.end_date})")
+            current = current.next
+        return " -> ".join(values)
+
+# --------------------------------------------------------------------------------------
+class Stack:
+    def __init__(self, capacity=None):
+        self.top = None
+        self.capacity = capacity
+        self.current_size = 0
+
+    def push(self, item):
+        if self.capacity and self.current_size >= self.capacity:
+            raise OverflowError("Stack is full.")
+        new_node = Node(item)
+        new_node.next = self.top
+        self.top = new_node
+        self.current_size += 1
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Pop from an empty stack.")
+        popped_data = self.top.data
+        self.top = self.top.next
+        self.current_size -= 1
+        return popped_data
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Peek from an empty stack.")
+        return self.top.data
+
+    def empty(self):
+        self.top = None
+        self.current_size = 0
+
+    def is_empty(self):
+        return self.top is None
+
+    def is_full(self):
+        if self.capacity is None:
+            return False
+        return self.current_size >= self.capacity
+
+    def size(self):
+        return self.current_size
+
+    def reverse(self):
+        prev = None
+        current = self.top
+        while current is not None:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.top = prev
+# --------------------------------------------------------------------------------------
+class CircularQueue:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.queue = [None] * capacity
+        self.front = -1
+        self.rear = -1
+
+    def is_empty(self):
+        return self.front == -1
+
+    def is_full(self):
+        return (self.rear + 1) % self.capacity == self.front
+
+    def enqueue(self, item):
+        if self.is_full():
+            raise OverflowError("Queue is full")
+
+        if self.is_empty():
+            self.front = 0
+        self.rear = (self.rear + 1) % self.capacity
+        self.queue[self.rear] = item
+
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+
+        item = self.queue[self.front]
+        self.queue[self.front] = None
+
+        if self.front == self.rear:
+            self.front = -1
+            self.rear = -1
+        else:
+            self.front = (self.front + 1) % self.capacity
+
+        return item
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        return self.queue[self.front]
+
+    def size(self):
+        if self.is_empty():
+            return 0
+        if self.rear >= self.front:
+            return self.rear - self.front + 1
+        return self.capacity - self.front + self.rear + 1
+
+    def __str__(self):
+        if self.is_empty():
+            return "Queue is empty"
+        if self.rear >= self.front:
+            return " -> ".join(map(str, self.queue[self.front: self.rear + 1]))
+        return " -> ".join(map(str, self.queue[self.front:] + self.queue[: self.rear + 1]))
+
+# --------------------------------------------------------------------------------------
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _parent(self, index: int) -> int:
+        return (index - 1) // 2
+
+    def _left_child(self, index: int) -> int:
+        return 2 * index + 1
+
+    def _right_child(self, index: int) -> int:
+        return 2 * index + 2
+
+    def _swap(self, index1: int, index2: int):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def _heapify_up(self, index: int):
+        while index > 0 and self.heap[self._parent(index)].name > self.heap[index].name:
+            self._swap(self._parent(index), index)
+            index = self._parent(index)
+
+    def _heapify_down(self, index: int):
+        smallest = index
+        left = self._left_child(index)
+        right = self._right_child(index)
+
+        if left < len(self.heap) and self.heap[left].name < self.heap[smallest].name:
+            smallest = left
+        if right < len(self.heap) and self.heap[right].name < self.heap[smallest].name:
+            smallest = right
+        if smallest != index:
+            self._swap(index, smallest)
+            self._heapify_down(smallest)
+
+    def insert(self, item):
+        self.heap.append(item)
+        self._heapify_up(len(self.heap) - 1)
+
+    def extract_min(self):
+        if len(self.heap) == 0:
+            raise IndexError("extract_min from empty heap")
+        self._swap(0, len(self.heap) - 1)
+        min_item = self.heap.pop()
+        self._heapify_down(0)
+        return min_item
+
+    def build_heap(self, items):
+        self.heap = items
+        for i in range(len(self.heap) // 2 - 1, -1, -1):
+            self._heapify_down(i)
+
+    def sorted(self):
+        sorted_list = []
+        while len(self.heap) > 0:
+            sorted_list.append(self.extract_min())
+        return sorted_list
--- Frontend/src/components/body/projectPage/Board.jsx
+++ Frontend/src/components/body/projectPage/Board.jsx
@@ -1,8 +1,62 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Column from "./Column";
 import Modal from "./Modal";
+import Card from "./Card";
+import DatePicker from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
 
-const Board = () => {
+// تابع Project به صورت جداگانه
+const Board = ({ projectId ,card}) => {
+  const [formData, setFormData] = useState({
+    name: card?.name || "",
+    start_date: card?.start_date || null,
+    end_date: card?.end_date || null,
+    employee_id: card?.employee_id || null ,
+    status: card?.status || "در حال انجام",
+    project_id: card?.project_id
+  });
+
+  const createNewTask = async (formData) => {
+    const { id: projectId } = useParams(); // Destructure id as projectId from URL params
+  
+    // Convert and assign project_id to formData
+    const updatedFormData = {
+      ...formData,
+      start_date: convertJalaliToGregorianISO(
+        convertPersianToEnglish(formData.start_date)
+      ),
+      end_date: convertJalaliToGregorianISO(
+        convertPersianToEnglish(formData.end_date)
+      ),
+      project_id: projectId,
+    };
+  
+    try {
+      const res = await fetch(`http://127.0.0.1:8000/tasks/${projectId}/tasks`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+        body: JSON.stringify(updatedFormData), // Use the updated formData
+      });
+  
+      if (res.status === 200) {
+        const resJson = await res.json();
+        console.log("Registration successful", resJson);
+        alert("Task created successfully");
+      } else {
+        const resJson = await res.json();
+        alert(resJson.detail);
+      }
+    } catch (e) {
+      console.log("Error:", e);
+      alert("An error occurred. Please try again.");
+    }
+  };
+
   const [isAccordionView, setIsAccordionView] = useState(false);
   const [expanded, setExpanded] = useState(null);
   const [columns, setColumns] = useState({
@@ -12,6 +66,19 @@ const Board = () => {
   });
   const [currentCard, setCurrentCard] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isAddCardModalVisible, setIsAddCardModalVisible] = useState(false);
+  const [newCardInfo, setNewCardInfo] = useState({
+    name: "",
+    description: "",
+    employee: "",
+    status: "todo",
+    startDate: null,
+    endDate: null,
+  });
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [isCommentModalVisible, setIsCommentModalVisible] = useState(false);
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
 
   // شناسایی حالت ریسپانسیو (آکاردئون یا سه ستونی)
   useEffect(() => {
@@ -25,34 +92,55 @@ const Board = () => {
     };
   }, []);
 
-  // افزودن کارت جدید
-  const addCard = (columnId) => {
-    const taskName = prompt("عنوان تسک را وارد کنید:", "تسک جدید");
-    if (!taskName) return;
-    const taskDescription = prompt("توضیحات تسک را وارد کنید:", "جزئیات تسک...");
-    if (!taskDescription) return;
-
-    const newCard = { id: Date.now(), name: taskName, description: taskDescription };
-    setColumns((prev) => ({
-      ...prev,
-      [columnId]: [...prev[columnId], newCard],
-    }));
+  const addCard = () => {
+    setNewCardInfo({
+      name: "",
+      description: "",
+      employee: "",
+      status: "todo",
+      startDate: null,
+      endDate: null,
+    });
+    setIsEditMode(false);
+    setIsAddCardModalVisible(true);
   };
 
-  // ویرایش کارت
+  const saveCard = () => {
+    if (!newCardInfo.name.trim() || !newCardInfo.description.trim()) return;
+
+    const targetColumnId = newCardInfo.status; // ستون مناسب بر اساس وضعیت
+
+    if (isEditMode) {
+      setColumns((prev) => {
+        const updatedColumns = {
+          ...prev,
+          [currentCard.status]: prev[currentCard.status].filter((card) => card.id !== currentCard.id),
+        };
+        updatedColumns[targetColumnId] = [
+          ...updatedColumns[targetColumnId],
+          { ...currentCard, ...newCardInfo },
+        ];
+        return updatedColumns;
+      });
+    } else {
+      const newCard = {
+        id: Date.now(), // نیاز به تغییر به شناسه یکتا داریم
+        ...newCardInfo,
+      };
+      setColumns((prev) => ({
+        ...prev,
+        [targetColumnId]: [...prev[targetColumnId], newCard],
+      }));
+    }
+    setIsAddCardModalVisible(false);
+    createNewTask();
+  };
+
   const editCard = (columnId, card) => {
-    const updatedName = prompt("عنوان جدید را وارد کنید:", card.name);
-    if (!updatedName) return;
-
-    const updatedDescription = prompt("توضیحات جدید را وارد کنید:", card.description);
-    if (!updatedDescription) return;
-
-    setColumns((prev) => ({
-      ...prev,
-      [columnId]: prev[columnId].map((c) =>
-        c.id === card.id ? { ...c, name: updatedName, description: updatedDescription } : c
-      ),
-    }));
+    setCurrentCard(card);
+    setNewCardInfo({ ...card });
+    setIsEditMode(true);
+    setIsAddCardModalVisible(true);
   };
 
   // حذف کارت
@@ -61,18 +149,6 @@ const Board = () => {
       ...prev,
       [columnId]: prev[columnId].filter((c) => c.id !== cardId),
     }));
-  };
-
-  // نمایش مودال برای نظرات
-  const openCommentModal = (card) => {
-    setCurrentCard(card);
-    setIsModalVisible(true);
-  };
-
-  // بستن مودال
-  const closeCommentModal = () => {
-    setCurrentCard(null);
-    setIsModalVisible(false);
   };
 
   return (
@@ -99,10 +175,9 @@ const Board = () => {
                         : "انجام شده"
                     }
                     cards={columns[key]}
-                    onAddCard={() => addCard(key)}
+                    onAddCard={key === "todo" ? addCard : null}
                     onEdit={(card) => editCard(key, card)}
                     onDelete={(cardId) => deleteCard(key, cardId)}
-                    onOpenModal={openCommentModal}
                     bgColor={
                       key === "todo"
                         ? "bg-gradient-to-tr from-pink-400 to-pink-600"
@@ -119,42 +194,189 @@ const Board = () => {
       ) : (
         // حالت سه ستونی برای صفحات بزرگ
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          <Column
-            title="کارها"
-            cards={columns.todo}
-            onAddCard={() => addCard("todo")}
-            onEdit={(card) => editCard("todo", card)}
-            onDelete={(cardId) => deleteCard("todo", cardId)}
-            onOpenModal={openCommentModal}
-            bgColor="bg-gradient-to-tr from-pink-400 to-pink-600"
-          />
-          <Column
-            title="در حال انجام"
-            cards={columns.doing}
-            onAddCard={() => addCard("doing")}
-            onEdit={(card) => editCard("doing", card)}
-            onDelete={(cardId) => deleteCard("doing", cardId)}
-            onOpenModal={openCommentModal}
-            bgColor="bg-gradient-to-tr from-yellow-300 to-yellow-500"
-          />
-          <Column
-            title="انجام شده"
-            cards={columns.done}
-            onAddCard={() => addCard("done")}
-            onEdit={(card) => editCard("done", card)}
-            onDelete={(cardId) => deleteCard("done", cardId)}
-            onOpenModal={openCommentModal}
-            bgColor="bg-gradient-to-tr from-green-400 to-green-600"
-          />
+          {Object.keys(columns).map((key) => (
+            <Column
+              key={key}
+              title={
+                key === "todo"
+                  ? "کارها"
+                  : key === "doing"
+                  ? "در حال انجام"
+                  : "انجام شده"
+              }
+              cards={columns[key]}
+              onAddCard={key === "todo" ? addCard : null}
+              onEdit={(card) => editCard(key, card)}
+              onDelete={(cardId) => deleteCard(key, cardId)}
+              bgColor={
+                key === "todo"
+                  ? "bg-gradient-to-tr from-pink-400 to-pink-600"
+                  : key === "doing"
+                  ? "bg-gradient-to-tr from-yellow-300 to-yellow-500"
+                  : "bg-gradient-to-tr from-green-400 to-green-600"
+              }
+            />
+          ))}
         </div>
       )}
 
-      {/* مودال نظرات */}
-      {isModalVisible && (
-        <Modal
-          card={currentCard}
-          onClose={closeCommentModal}
-        />
+      {isAddCardModalVisible && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+            <h3 className="text-lg font-bold mb-4">
+              {isEditMode ? "ویرایش کارت" : "افزودن کارت جدید"}
+            </h3>
+            <input
+              type="text"
+              placeholder="عنوان کارت"
+              value={newCardInfo.name}
+              onChange={(e) =>
+                setNewCardInfo((prev) => ({ ...prev, name: e.target.value }))
+              }
+              className="w-full border border-gray-300 rounded-lg p-2 mb-4"
+            />
+            <textarea
+              placeholder="توضیحات کارت"
+              value={newCardInfo.description}
+              onChange={(e) =>
+                setNewCardInfo((prev) => ({
+                  ...prev,
+                  description: e.target.value,
+                }))
+              }
+              className="w-full border border-gray-300 rounded-lg p-2 mb-4"
+              rows="3"
+            />
+            <input
+              type="text"
+              placeholder="نام کارمند"
+              value={newCardInfo.employee}
+              onChange={(e) =>
+                setNewCardInfo((prev) => ({
+                  ...prev,
+                  employee: e.target.value,
+                }))
+              }
+              className="w-full border border-gray-300 rounded-lg p-2 mb-4"
+            />
+            <div className="grid grid-cols-2 gap-4 mb-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  تاریخ شروع
+                </label>
+                <DatePicker
+                  value={newCardInfo.startDate}
+                  onChange={(date) =>
+                    setNewCardInfo((prev) => ({
+                      ...prev,
+                      startDate: date?.format("YYYY/MM/DD"),
+                    }))
+                  }
+                  calendar={persian}
+                  locale={persian_fa}
+                  inputClass="mt-1 block w-full rounded-lg border-gray-300 shadow-md focus:ring-indigo-500 focus:border-indigo-500"
+                  format="YYYY/MM/DD"
+                  placeholder="تاریخ شروع را انتخاب کنید"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  تاریخ پایان
+                </label>
+                <DatePicker
+                  value={newCardInfo.endDate}
+                  onChange={(date) =>
+                    setNewCardInfo((prev) => ({
+                      ...prev,
+                      endDate: date?.format("YYYY/MM/DD"),
+                    }))
+                  }
+                  calendar={persian}
+                  locale={persian_fa}
+                  inputClass="mt-1 block w-full rounded-lg border-gray-300 shadow-md focus:ring-indigo-500 focus:border-indigo-500"
+                  format="YYYY/MM/DD"
+                  placeholder="تاریخ پایان را انتخاب کنید"
+                />
+              </div>
+            </div>
+            <select
+              value={newCardInfo.status}
+              onChange={(e) =>
+                setNewCardInfo((prev) => ({
+                  ...prev,
+                  status: e.target.value,
+                }))
+              }
+              className="w-full border border-gray-300 rounded-lg p-2 mb-4"
+            >
+              <option value="todo">معلق</option>
+              <option value="doing">در حال انجام</option>
+              <option value="done">انجام شده</option>
+            </select>
+            <div className="flex justify-end space-x-4">
+              <button
+                className="bg-red-500 text-white text-sm py-2 px-4 rounded hover:bg-red-600 transition"
+                onClick={() => setIsAddCardModalVisible(false)}
+              >
+                لغو
+              </button>
+              <button
+                className="bg-blue-500 text-white text-sm py-2 px-4 rounded hover:bg-blue-600 transition"
+                onClick={saveCard}
+              >
+               ذخیره 
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* دکمه نظردهی */}
+      <button
+        className="fixed bottom-4 left-4 bg-blue-600 text-white p-3 rounded-full shadow-lg hover:bg-blue-700 transition"
+        onClick={() => setIsCommentModalVisible(true)}
+      >
+        نظر دهید
+      </button>
+      {isCommentModalVisible && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
+            <h3 className="text-lg font-bold mb-4">ارسال نظر</h3>
+            <textarea
+              placeholder="نظر خود را بنویسید..."
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              className="w-full border border-gray-300 rounded-lg p-2 mb-4"
+              rows="4"
+            />
+            <div className="flex justify-between">
+              <button
+                className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition"
+                onClick={() => setIsCommentModalVisible(false)}
+              >
+                بستن
+              </button>
+              <button
+                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
+                onClick={() => {
+                  if (comment.trim()) {
+                    setComments((prev) => [comment, ...prev]);
+                    setComment("");
+                  }
+                }}
+              >
+                ارسال
+              </button>
+            </div>
+            <div className="mt-4">
+              {comments.map((c, index) => (
+                <p key={index} className="border-b border-gray-300 pb-2 mb-2">
+                  {c}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
